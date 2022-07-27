ff7d6a41a58ebada7eef3c0895b1893d4b53acb8
DreadkingRathalos <108301431+DreadkingRathalos@users.noreply.github.com>
2022-07-27 13:48:13 -0500

    Create Database_App.py

---
+++ Database_App.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+frame = tk.Frame(master=window, width=200, height=200)
+frame.pack()
+
+label1 = tk.Label(master=frame, text="Monster Class")
+label1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Flying Wyvern", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Fanged Wyvern", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Brute Wyvern", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Bird Wyvern", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Piscine Wyvern", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Levianth", bg="yellow")
+button1.place(x=00, y=0)
+
+button1 = tk.Label(master=frame, text="Fanged Beast", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Amphian", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Carapaceon", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Elder Dragon", bg="yellow")
+button1.place(x=0, y=0)
+
+button1 = tk.Label(master=frame, text="Other", bg="yellow")
+button1.place(x=0, y=0)
+
+window.mainloop()
